d357c4695dca8a51d384e8c151c06ddde639e654
sampanggabean <125780491+sampanggabean@users.noreply.github.com>
2023-05-31 23:31:44 +0700

    Api (#12)
    * requirements file
    
    * Change connection to django db
    
    * API with get all endpoint
    
    * Add password
    
    * Fix scraper
    
    * pdi update
    
    * rename
    
    * Filter capabilities

--- job_scraper_api/api/views.py
+++ job_scraper_api/api/views.py
@@ -3,11 +3,38 @@ from rest_framework.decorators import api_view
 from .models import Job
 from .serializers import JobSerializer
 from rest_framework.pagination import PageNumberPagination
+from django.db.models import Q
 
 # Create your views here.
 @api_view(['GET'])
-def getAllData(request):
+def getJobData(request):
+
+    # check query params
+    queries = request.GET.get('query')
+    queries = queries.split(',')
+    date_mode = request.GET.get('date_mode')
+    date = request.GET.get('date')
+    location = request.GET.get('location')
+    company = request.GET.get('company')
+
     jobs = Job.objects.all()
+
+    if len(queries):
+        q_objects = Q()
+        for query in queries:
+            q_objects |= Q(query__icontains=query)
+        jobs = jobs.filter(q_objects)
+        
+    if date_mode and date: 
+        if date_mode == 'before':
+            jobs = jobs.filter(created_at__lt=date)
+        if date_mode == 'after':
+            jobs = jobs.filter(created_at__gt=date)
+    if location:
+        jobs = jobs.filter(location=location)
+    if company:
+        jobs = jobs.filter(Q(company__icontains=company))
+
     paginator = PageNumberPagination()
     paged_jobs = paginator.paginate_queryset(jobs, request)
     serializer = JobSerializer(paged_jobs, many = True)
